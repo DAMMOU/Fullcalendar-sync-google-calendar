65b98e79d3020dcfcd2cbd19fcf555edf46a3865
DAMMOU <udammou@gmail.com>
2023-08-31 19:40:51 +0200

    Add the auth, routes, and the associated pages

---
+++ resources/views/events/list.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+    @push('css')
+        <style>
+        h1{
+            color:red;
+        }
+    </style>
+    @endpush
+@section('content')
+   <h1>Youusef DAMMOU</h1> 
+@endsection
